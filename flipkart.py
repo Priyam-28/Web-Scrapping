7480cd75c8a0b76d805bea77f21ec059286de0b4
UnknownRider <barikpriyam28@gmail.com>
2024-05-29 00:27:25 +0530

    Logged Reviews and Name

--- flipkart.py
+++ flipkart.py
@@ -1,7 +1,27 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-search_url = f"https://www.flipkart.com/search?q="+"iphone12pro"
+search_url = "https://www.flipkart.com/search?q="+"iphone12pro"
 response = requests.get(search_url)
 
-print(response.text)
+flipkart_page=response.text
+
+flipkart_html=bs(flipkart_page,'html.parser');
+
+
+bigbox=flipkart_html.findAll('div',{"class":{"cPHDOP col-12-12"}}) 
+# finding all the class having key as class and values as cPHDOP col-12-12
+del bigbox[0:2]
+productlink="https://www.flipkart.com"+bigbox[1].div.div.div.a['href']
+product_req=requests.get(productlink)
+product_html=(bs(product_req.text,'html.parser'))
+
+comment_box=product_html.findAll('div',{"class":{"col EPCmJX"}})
+# print(comment_box[1].findAll('p',{"class":"_2NsDsF AwS1CA"}))
+
+# for i in bigbox:
+#     print("https://www.flipkart.com"+i.div.div.div.a['href'])
+
+for i in comment_box:
+    print(comment_box[2].div.div.findAll('p',{"class":{"_2NsDsF AwS1CA"}}))
+    
